4b5299b31e767ffe0fd1d7b0d32f58d3a7f7b4a5
luzty <luzty1402@gmail.com>
2017-10-14 17:46:34 -0500

    add hosting

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1zv+vlfjiixdpbi%%-+mpz0$nt4rb%@os^jcqz==(%_ik$k^vk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<tu-usuario>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'luzty.pythonanywhere.com']
 
 
 # Application definition
